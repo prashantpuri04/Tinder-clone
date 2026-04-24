b4292d63ecf5b119032b4a6aabe5c0fcbe9f3379
prashantpuri <prashantpuri04@gmail.com>
2026-04-24 16:03:41 +0530

    routes-added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/user",(req,res)=>{
+    res.send({
+        name:"John Doe",
+        age:30,
+        email:"abc.com"
+    });
+});
+
+app.post("/user",(req,res)=>{
+    res.send({
+        message:"User created successfully!" ,
+    })
+});
+app.put("/user",(req,res)=>{
+    res.send({
+        message:"User updated successfully!" ,
+    })
+}); 
+app.delete("/user",(req,res)=>{
+    res.send({
+        message:"User deleted successfully!" ,  
+    })
+});
+
 app.use((req,res)=>{
     res.send("Hello World from the server!");
 });
